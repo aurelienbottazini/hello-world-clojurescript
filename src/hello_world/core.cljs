f27b87568bd069155f3f413c666aea8ef1fef0c6
Aurélien Bottazini <aurelien@bottazini.com>
2019-04-05 15:06:19 +0200

    WIP devcards

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,8 +1,19 @@
 (ns hello-world.core
-  (:require react-dom))
+  (:require [devcards.core] [react-dom])
+  (:require-macros
+   [cljs.test :refer [is testing]]
+   [ devcards.core :refer [defcard deftest dom-node]]))
 
-(.render js/ReactDOM
-         (.createElement js/React "h2" nil "Hello, React world!")
-         (.getElementById js/document "app"))
+(defcard app-card
+  "Example card" "foo")
+
+(deftest sample-test
+  (testing "happy test"
+    (is (= 1 1))))
+
+(when-let [app-elem (js/document.querySelector "#app")]
+  (.render js/ReactDOM
+           (.createElement js/React "h2" nil "Hello, React world!")
+           (.getElementById js/document "app")))
 
 (+ 1 2 3)
